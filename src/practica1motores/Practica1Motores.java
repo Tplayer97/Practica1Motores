a66bc67eea90188aeebd92cf4dfafc5874ffc07d
Tplayer <34649536+Tplayer97@users.noreply.github.com>
2020-11-03 16:23:33 +0100

    Lector hecho
    FUNCIONANDO

--- src/practica1motores/Practica1Motores.java
+++ src/practica1motores/Practica1Motores.java
@@ -5,6 +5,12 @@
  */
 package practica1motores;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.common.SolrInputDocument;
+
 /**
  *
  * @author Tplayer
@@ -14,8 +20,20 @@ public class Practica1Motores {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException, SolrServerException {
+        
+        HttpSolrClient solr = new HttpSolrClient.Builder("http://localhost:8983/solr/micoleccion").build();
+        SolrInputDocument docu = new SolrInputDocument();
+        Lector leer = new Lector();
+        
+        leer.leer("LISA0.001");
+        ArrayList<SolrInputDocument> doc = new ArrayList<SolrInputDocument>();
+        doc = leer.getdoc();
+        for (int i = 0; i < doc.size(); i++) {
+            docu = doc.get(i);
+            solr.add(docu);
+        }
+            solr.commit();
     }
     
 }
